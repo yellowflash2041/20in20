6ed2d7a48d10028a9f6028180df1a4fe3b31c9ee
yellowflash2041 <yellowflash2041@gmail.com>
2022-11-18 14:35:31 +0900

    Update App

--- week20/demo/frontend/src/App.vue
+++ week20/demo/frontend/src/App.vue
@@ -13,29 +13,29 @@
 </template>
 
 <script>
-import Todos from '@/components/Todos.vue';
-import AddTodo from '@/components/AddTodo.vue';
-import FilterTodos from '@/components/FilterTodos.vue'
-export default {
-  name: "App",
-  components: {
-    Todos,
-    AddTodo,
-    FilterTodos
-  }
-};
+  import Todos from '@/components/Todos.vue';
+  import AddTodo from '@/components/AddTodo.vue';
+  import FilterTodos from '@/components/FilterTodos.vue'
+  export default {
+    name: "App",
+    components: {
+      Todos,
+      AddTodo,
+      FilterTodos
+    }
+  };
 </script>
 
 <style>
-body {
-  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-  line-height: 1.6;
-  background: #e8f7f0
-}
-.container {
-  max-width: 1100px;
-  margin: auto;
-  overflow: auto;
-  padding: 0 2rem;
-}
+  body {
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    line-height: 1.6;
+    background: #e8f7f0
+  }
+  .container {
+    max-width: 1100px;
+    margin: auto;
+    overflow: auto;
+    padding: 0 2rem;
+  }
 </style>
